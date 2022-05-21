3026e72a6e8ea1614344e74030e3884e8c3acbcd
Linus Sherwin <99864722+linus-sherwin@users.noreply.github.com>
2022-05-21 15:22:02 -0400

    Add files via upload

--- Simple_stock.py
+++ Simple_stock.py
@@ -4,15 +4,18 @@ import pandas as pd
 
 st.write(""" 
          # Simple Stock Price App
-         Shown are the stock closing price and volume of ***Apple***
+         Retrieve the stock closing price and volume
          """)
 
 # define ticker symbol
-tickerSymbol = 'AAPL'
+st.header('Enter the Company Code')
+inputsymbol = 'AAPL'
+
+tickerSymbol = st.text_area("Input", inputsymbol, height=10)
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-5-31')
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2022-4-30')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.write(""" 
